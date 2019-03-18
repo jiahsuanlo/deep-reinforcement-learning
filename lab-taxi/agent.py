6cbb5c8a2740ecb419cb14d8083f5d50ab300c17
jiahsuanlo <jiahsuanlo@yahoo.com>
2019-03-17 20:46:01 -0700

    implemented taxi agent using Q-Learning algorithm (SARSAMAX)

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -2,6 +2,8 @@ import numpy as np
 from collections import defaultdict
 
 class Agent:
+    """ Q-Learning agent
+    """
 
     def __init__(self, nA=6):
         """ Initialize agent.
@@ -9,12 +11,19 @@ class Agent:
         Params
         ======
         - nA: number of actions available to the agent
+        - eps: parameter for epsilon-greedy policy
+        - gamma: discount of reward
+        - alpha: learning rate
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.eps= 0.001
+        self.gamma= 0.99
+        self.alpha= 0.3
 
     def select_action(self, state):
-        """ Given the state, select an action.
+        """ Given the state, select an action, based on epsilon greedy
+        policy
 
         Params
         ======
@@ -24,10 +33,17 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        # generate action probablity
+        action_prob= np.ones(self.nA)*self.eps/self.nA
+        best_action= np.argmax(self.Q[state])
+        action_prob[best_action]+= 1- self.eps
+        
+        return np.random.choice(self.nA, p= action_prob)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
+        
+        Based on the Q-Learning algorithm (SARSAMAX)
 
         Params
         ======
@@ -37,4 +53,6 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        target= reward + self.gamma*np.max(self.Q[next_state])
+                
+        self.Q[state][action] += self.alpha*(target - self.Q[state][action])
